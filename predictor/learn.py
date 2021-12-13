6489da59b3fe452e22bd7f143b0e18f9e4921579
root <root@Katzenbuckel.localdomain>
2021-12-13 14:03:15 +0100

    first commit; added model

---
+++ predictor/learn.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+import numpy as np
+import pickle
+
+data = pd.read_csv("collected/dataset.csv",delimiter=';')
+
+x = []
+y = []
+
+for s,a in zip(data.stars,data.acc):
+    x.append([s,a])
+
+for pp in data.pp:
+    y.append(pp)
+
+regr = RandomForestRegressor(n_estimators=10, max_features=2)
+regr.fit(x, y)
+
+with open("model.pkl", "wb") as file:
+    pickle.dump(regr,file)
+
+def plot(stars,model):
+    stardata = [model.predict([[stars,i / 100]])[0] for i in range(40,100)]
+    plt.ylabel("PP")
+    plt.xlabel("Accuracy")
+    plt.plot(stardata)
+    plt.show()
