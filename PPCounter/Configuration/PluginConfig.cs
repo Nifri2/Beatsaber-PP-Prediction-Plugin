68af8af4904491a12c5d445d3da6d86f2d1ef033
Mienfoo <fearl@DESKTOP-5B8T8C4>
2021-12-13 22:19:51 -0600

    PPCounter Plugin!!

---
+++ PPCounter/Configuration/PluginConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace PPCounter.Settings
+{
+    internal class PluginConfig
+    {
+        public static PluginConfig Instance { get; set; }
+        public virtual bool ppBuildsUp { get; set; } = false;
+        public virtual int port { get; set; } = 5000;
+    }
+}
---
+++ PPCounter/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using IPALogger = IPA.Logging.Logger;
+using PPCounter.Settings;
+
+
+namespace PPCounter
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public void Init(Config conf, IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            PluginConfig.Instance = conf.Generated<PluginConfig>();
+            
+            Log.Info("[!] PPCounter initialized.");
+        }
+
+        [OnStart]
+        public void OnApplicationStart() { }
+
+        [OnExit]
+        public void OnApplicationQuit() {  }
+    }
+}
